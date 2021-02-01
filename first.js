457270be59f483c8020808b4383b5d0932ac0f77
Durga R <raod1989r@gmail.com>
2021-02-01 10:43:33 +0530

    updated text

--- first.js
+++ first.js
@@ -1,3 +1,10 @@
 Hi,
 
 This is not my first js, dont edit
+
+
+Finance minister Nirmala Sitharaman will present Union Budget 2021 at 11am on Monday in Parliament. This is the first budget after Covid-19 pandemic and the ninth one under the Modi government. For Nirmala Sitharaman, this is the third budget as she debuted in 2019, springing a change by replacing decades-old leather briefcase with 'bahi-khata'.
+
+Union Budget 2021 is unique in many regards. This will be the first paperless budget. The year 2020, as PM Modi said ahead of the joint session of Parliament on Friday, was a year of four to five mini budgets as the finance ministry announced several stimulus packages throughout the year to cushion the blow of the pandemic. Union Budget 2021 will be an extension of those, PM Modi said.
+
+The Economic Survey 2020-21 has projected that India's real GDP growth would be around 11 per cent in 2021-22. while the nominal GDP growth has been estimated to be around 15.4 per cent.
